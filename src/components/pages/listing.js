784a60abe7da69f2a28434735ee4dc1c6ab9e805
Erum Dhukka <erumdhukka531@gmail.com>
2021-04-21 02:26:55 -0500

    added .js pages. need to link buttons

--- src/components/pages/listing.js
+++ src/components/pages/listing.js
@@ -1,28 +1,121 @@
-// import {listing} from 'reactnative-ui-bootstrap'
+import React from 'react';
+import { View, Text, Image, Pressable } from 'react-native';
+// import styles from './styles.js';
+import { useNavigation } from '@react-navigation/native';
 
-      // return(
-      //   <Div className={'row'}>
-      //     <Div className={'col-6 col-md-9'}>
-      //       ...Some content here
-      //     </Div>
-      //   </Div>
-      // )
+// const days = 7;
 
-      // function Rent() {
-      //   return (
-      //     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      //       <Text>Listing!</Text>
-      //     </View>
-      //   );
-      // }
+const Post = (props) => {
+  const post = props.post;
 
-      const Listing = () => (
-        <div>
-          <h1>Listing</h1>
-          <p>
-          
-          </p>
-        </div>
-      );
-      
-      export default Listing;
+  const navigation = useNavigation();
+
+  const goToPostPage = () => {
+    navigation.navigate('Post', { postId: post.id });
+  };
+
+  return (
+   <View> 
+    <Pressable
+    style={styles.searchButton}
+    onPress={() => navigation.navigate('Destination Search')}>
+    <Fontisto name="search" size={25} color={'#f15454'} />
+    <Text style={styles.searchButtonText}>zipcode</Text>
+  </Pressable>
+
+    <Pressable onPress={goToPostPage} style={styles.container}>
+      {/* Image  */}
+      <Image style={styles.image} source={{ uri: post.image }} />
+
+      {/* Bed & Bedroom  */}
+      <Text style={styles.bedrooms}>
+        {post.bed} bed {post.bathroom} bathroom
+      </Text>
+
+      {/* Type & Description */}
+      <Text style={styles.description} numberOfLines={2}>
+      {/* <Text > */}
+        {post.type}. {post.shared}
+      </Text>
+
+      {/*  price */}
+      {/* <Text style={styles.prices}> */}
+      {/* <Text style={styles.oldPrice}>${post.oldPrice}</Text> */}
+      <Text style={styles.price}> ${post.Price}/month </Text>
+      <Text style={styles.dateAvailable}> ${post.dateAvailable} </Text>
+      <Text style={styles.pet}> ${post.Pet} </Text>
+      <Text style={styles.parking}> ${post.Parking} </Text>
+      <Text style={styles.washer}> ${post.washer} </Text>
+      <Text style={styles.wifi}> ${post.Wifi} </Text>
+      <Text style={styles.stove}> ${post.stove} </Text>
+
+      {/* </Text> */}
+    </Pressable>
+   </View>
+  );
+};
+
+// export default Post;
+//       const Listing = () => (
+//         <div>
+//           <h1>Listing</h1>
+//           <p>
+
+//           </p>
+//         </div>
+//       );
+
+export default Post;
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 20,
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 3 / 2,
+    resizeMode: 'cover',
+    borderRadius: 10,
+  },
+
+  bedrooms: {
+    marginVertical: 10,
+    color: '#5b5b5b',
+  },
+  description: {
+    fontSize: 18,
+    lineHeight: 26,
+  },
+  prices: {
+    fontSize: 18,
+    marginVertical: 10,
+  },
+  oldPrice: {
+    color: '#5b5b5b',
+    textDecorationLine: 'line-through',
+  },
+  price: {
+    fontWeight: 'bold',
+  },
+  totalPrice: {
+    color: '#5b5b5b',
+    textDecorationLine: 'underline',
+  },
+  searchButton: {
+    backgroundColor: '#fff',
+    height: 60,
+    width: Dimensions.get('screen').width - 20,
+    borderRadius: 30,
+    marginHorizontal: 10,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    top: 50,
+    zIndex: 100,
+  },
+  searchButtonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
--- src/components/pages/profile.js
+++ src/components/pages/profile.js
@@ -1,11 +1,205 @@
 import React from "react";
 
+import {View, SafeAreaView, StyleSheet} from 'react-native';
+import {
+  Avatar,
+  Title,
+  Caption,
+  Text,
+  TouchableRipple,
+} from 'react-native-paper';
+
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import Share from 'react-native-share';
+
+
+import files from '../assets/filesBase64';
+
+const ProfileScreen = () => {
+
+  const myCustomShare = async() => {
+    const shareOptions = {
+      url: files.appLogo,
+      // urls: [files.image1, files.image2]
+    }
+
+    try {
+      const ShareResponse = await Share.open(shareOptions);
+      console.log(JSON.stringify(ShareResponse));
+    } catch(error) {
+      console.log('Error => ', error);
+    }
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+
+      <View style={styles.userInfoSection}>
+        <View style={{flexDirection: 'row', marginTop: 15}}>
+          <Avatar.Image 
+            source={{
+              uri: 'https://api.adorable.io/avatars/80/abott@adorable.png',
+            }}
+            size={80}
+          />
+          <View style={{marginLeft: 20}}>
+            <Title style={[styles.title, {
+              marginTop:15,
+              marginBottom: 5,
+            }]}>John Doe</Title>
+            <Caption style={styles.caption}>@j_doe</Caption>
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.userInfoSection}>
+        <View style={styles.row}>
+          <Icon name="map-marker-radius" color="#777777" size={20}/>
+          <Text style={{color:"#777777", marginLeft: 20}}>Houston, TX</Text>
+        </View>
+        <View style={styles.row}>
+          <Icon name="phone" color="#777777" size={20}/>
+          <Text style={{color:"#777777", marginLeft: 20}}>+1-123-456-7890</Text>
+        </View>
+        <View style={styles.row}>
+          <Icon name="email" color="#777777" size={20}/>
+          <Text style={{color:"#777777", marginLeft: 20}}>john_doe@email.com</Text>
+        </View>
+      </View>
+
+      <View style={styles.infoBoxWrapper}>
+          <View style={[styles.infoBox, {
+            borderRightColor: '#dddddd',
+            borderRightWidth: 1
+          }]}>
+            <Title>Add Listing</Title>
+            <TouchableRipple onPress={() => {}}>
+              <View style={styles.menuItem}>
+                <Icon name="plus-add-more-detail" color="#FF6347" size={30}/>
+                <Text style={styles.menuItemText}>Add Listing </Text>
+              </View>
+             </TouchableRipple>
+            
+          </View>
+          <View style={styles.infoBox}>
+            {/* <Title>Edit Post</Title> */}
+            <TouchableRipple onPress={() => {}}>
+              <View style={styles.menuItem}>
+                <Icon name="account-check-outline" color="#FF6347" size={30}/>
+                <Text style={styles.menuItemText}>Edit Profile </Text>
+              </View>
+             </TouchableRipple>
+          </View>
+      </View>
+
+      <View style={styles.menuWrapper}>
+       
+      </View>
+    </SafeAreaView>
+  );
+};
+
+
+
 function Profile() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Profile!</Text>
-      </View>
-    );
-  }
+      // <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      //   <Text>Profile!</Text>
+      // </View>
+          // headerRight: () => (
+          //   <View style={{flexDirection: 'row', marginRight: 10}}>
+          //     <Icon.Button
+          //       name="ios-search"
+          //       size={25}
+          //       color={colors.text}
+          //       backgroundColor={colors.background}
+          //       onPress={() => {}}
+          //     />
+          //     <TouchableOpacity
+          //       style={{paddingHorizontal: 10, marginTop: 5}}
+          //       onPress={() => {
+          //         navigation.navigate('Profile');
+          //       }}>
+          //       <Avatar.Image
+          //         source={{
+          //           uri:
+          //             'https://api.adorable.io/avatars/80/abott@adorable.png',
+          //         }}
+          //         size={30}
+          //       />
+          //     </TouchableOpacity>
+          //   </View>
+          // ),
+    
+    // )
+  // }
+
+
+      const ProfileStackScreen = ({navigation}) => {
+        headerRight: () => (
+          <View style={{marginRight: 10}}>
+            <MaterialCommunityIcons.Button
+              name="account-edit"
+              size={25}
+              backgroundColor={colors.background}
+              color={colors.text}
+              onPress={() => navigation.navigate('EditProfile')}
+            />
+          </View>
+        ),
+      };
+
 
   export default Profile;
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    userInfoSection: {
+      paddingHorizontal: 30,
+      marginBottom: 25,
+    },
+    title: {
+      fontSize: 24,
+      fontWeight: 'bold',
+    },
+    caption: {
+      fontSize: 14,
+      lineHeight: 14,
+      fontWeight: '500',
+    },
+    row: {
+      flexDirection: 'row',
+      marginBottom: 10,
+    },
+    infoBoxWrapper: {
+      borderBottomColor: '#dddddd',
+      borderBottomWidth: 1,
+      borderTopColor: '#dddddd',
+      borderTopWidth: 1,
+      flexDirection: 'row',
+      height: 100,
+    },
+    infoBox: {
+      width: '50%',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    menuWrapper: {
+      marginTop: 10,
+    },
+    menuItem: {
+      flexDirection: 'row',
+      paddingVertical: 15,
+      paddingHorizontal: 30,
+    },
+    menuItemText: {
+      color: '#777777',
+      marginLeft: 20,
+      fontWeight: '600',
+      fontSize: 16,
+      lineHeight: 26,
+    },
+  });
