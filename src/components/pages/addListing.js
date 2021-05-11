7493644a0bff3eaf684c41a01d4783f65a687322
Erum Dhukka <erumdhukka531@gmail.com>
2021-05-10 20:46:57 -0500

    listing form page connected

--- src/components/pages/addListing.js
+++ src/components/pages/addListing.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import {Text} from 'react-native';
 
 function AddListing() {
-
+return (
 <Text>Add Listing </Text>
 
 
-}
+)}
 
 
 export default AddListing; 
--- src/components/pages/profile.js
+++ src/components/pages/profile.js
@@ -1,4 +1,4 @@
-import { View, SafeAreaView, StyleSheet, ScrollView} from 'react-native';
+import { View, SafeAreaView, StyleSheet, ScrollView } from 'react-native';
 import {
   Avatar,
   Title,
@@ -6,7 +6,7 @@ import {
   Caption,
   Pressable,
   TouchableRipple,
-  Card, 
+  Card,
   Paragraph,
 } from 'react-native-paper';
 
@@ -19,8 +19,6 @@ import { useNavigation } from '@react-navigation/native';
 // import EditProfileScreen from './EditProfile';
 // import { Card } from 'react-native-elements';
 // import { Card, ListItem, Button } from 'react-native-elements'
-
-
 
 function Profile() {
   const navigation = useNavigation();
@@ -40,84 +38,85 @@ function Profile() {
 
   return (
     <SafeAreaView style={styles.container}>
-        <ScrollView>
-      <View style={styles.userInfoSection}>
-        <View style={{ flexDirection: 'row', marginTop: 15 }}>
-          {/* <Avatar.Image
+      <ScrollView>
+        <View style={styles.userInfoSection}>
+          <View style={{ flexDirection: 'row', marginTop: 15 }}>
+            {/* <Avatar.Image
             source={{
               uri: 'https://api.adorable.io/avatars/80/abott@adorable.png',
             }}
             size={80}
           /> */}
-          <View style={{ marginLeft: 20}}>
-            <Title
-              style={[
-                styles.title,
-                {
-                  marginTop: 15,
-                  marginBottom: 5,
-                },
-              ]}
-            >
-              John Doe
-            </Title>
-            <Caption style={styles.caption}>@j_doe</Caption>
+            <View style={{ marginLeft: 20 }}>
+              <Title
+                style={[
+                  styles.title,
+                  {
+                    marginTop: 15,
+                    marginBottom: 5,
+                  },
+                ]}
+              >
+                John Doe
+              </Title>
+              <Caption style={styles.caption}>@j_doe</Caption>
+            </View>
           </View>
         </View>
-      </View>
 
-      <View style={styles.userInfoSection}>
-        <View style={styles.row}>
-          <Icon name="map-marker-radius" color="#777777" size={20} />
-          <Text style={{ color: '#777777', marginLeft: 20 }}>Houston, TX</Text>
+        <View style={styles.userInfoSection}>
+          <View style={styles.row}>
+            <Icon name="map-marker-radius" color="#777777" size={20} />
+            <Text style={{ color: '#777777', marginLeft: 20 }}>
+              Houston, TX
+            </Text>
+          </View>
+          <View style={styles.row}>
+            <Icon name="phone" color="#777777" size={20} />
+            <Text style={{ color: '#777777', marginLeft: 20 }}>
+              +1-123-456-7890
+            </Text>
+          </View>
+          <View style={styles.row}>
+            <Icon name="email" color="#777777" size={20} />
+            <Text style={{ color: '#777777', marginLeft: 20 }}>
+              john_doe@email.com
+            </Text>
+          </View>
         </View>
-        <View style={styles.row}>
-          <Icon name="phone" color="#777777" size={20} />
-          <Text style={{ color: '#777777', marginLeft: 20 }}>
-            +1-123-456-7890
-          </Text>
-        </View>
-        <View style={styles.row}>
-          <Icon name="email" color="#777777" size={20} />
-          <Text style={{ color: '#777777', marginLeft: 20 }}>
-            john_doe@email.com
-          </Text>
-        </View>
-      </View>
 
-      <View style={styles.infoBoxWrapper}>
-        <View
-          style={[
-            styles.infoBox,
-            {
-              borderRightColor: '#dddddd',
-              borderRightWidth: 1,
-            },
-          ]}
-        >
-          <Title></Title>
-          <TouchableRipple onPress={() => {}}>
-            <View style={styles.menuItem}>
-              <Icon name="plus-add-more-detail" color="#FF6347" size={30} />
-              <Text style={styles.menuItemText}>Add Listing </Text>
-            </View>
-          </TouchableRipple>
+        <View style={styles.infoBoxWrapper}>
+          <View
+            style={[
+              styles.infoBox,
+              {
+                borderRightColor: '#dddddd',
+                borderRightWidth: 1,
+              },
+            ]}
+          >
+            <Title></Title>
+            <TouchableRipple onPress={() => navigation.navigate('AddListing')}>
+              <View style={styles.menuItem}>
+                <Icon name="plus-add-more-detail" color="#FF6347" size={30} />
+                <Text style={styles.menuItemText}>Add Listing </Text>
+              </View>
+            </TouchableRipple>
+          </View>
+          <View style={styles.infoBox}>
+            {/* <Title>Edit Post</Title> */}
+            <TouchableRipple onPress={() => {}}>
+              <View style={styles.menuItem}>
+                <Icon name="account-check-outline" color="#FF6347" size={30} />
+                <Text style={styles.menuItemText}>Edit Profile </Text>
+              </View>
+            </TouchableRipple>
+          </View>
         </View>
-        <View style={styles.infoBox}>
-          {/* <Title>Edit Post</Title> */}
-          <TouchableRipple onPress={() => {}}>
-            <View style={styles.menuItem}>
-              <Icon name="account-check-outline" color="#FF6347" size={30} />
-              <Text style={styles.menuItemText}>Edit Profile </Text>
-            </View>
-          </TouchableRipple>
-        </View>
-      </View>
 
-      <View style={styles.menuWrapper}></View>
+        <View style={styles.menuWrapper}></View>
 
-
-      {/* <View style={styles.container}>
+        {/* <View style={styles.container}>
                 <SingleCardView
                     elevation={1}
                     shadowColor="rgb(50,50,50)"
@@ -131,37 +130,33 @@ function Profile() {
                 </SingleCardView>
             </View> */}
 
-    <Card>
-        <Card.Content style={styles.card}>
-        <Card.Cover source={{ uri: 'https://images.unsplash.com/photo-1484154218962-a197022b5858?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1506&q=80/700' }} />
-          <Title name='address'>6100 Main St, Houston, TX 77005</Title>
-          <Paragraph>Card content</Paragraph>
-        </Card.Content>
-      </Card>
+        <Card>
+          <Card.Content style={styles.card}>
+            <Card.Cover
+              source={{
+                uri: 'https://images.unsplash.com/photo-1484154218962-a197022b5858?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1506&q=80/700',
+              }}
+            />
+            <Title name="address">6100 Main St, Houston, TX 77005</Title>
+            <Paragraph>Card content</Paragraph>
+          </Card.Content>
+        </Card>
 
-      <Card>
-        <Card.Content>
-        <Card.Cover source={{ uri: 'https://images.unsplash.com/photo-1513584684374-8bab748fbf90?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1492&q=80' }} />
-          <Title name='address'>6245 Main St, Houston, TX 77010</Title>
-          <Paragraph>Card content</Paragraph>
-        </Card.Content>
-      </Card>
-     
+        <Card>
+          <Card.Content>
+            <Card.Cover
+              source={{
+                uri: 'https://images.unsplash.com/photo-1513584684374-8bab748fbf90?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1492&q=80',
+              }}
+            />
+            <Title name="address">6245 Main St, Houston, TX 77010</Title>
+            <Paragraph>Card content</Paragraph>
+          </Card.Content>
+        </Card>
       </ScrollView>
     </SafeAreaView>
-
-
-    
-
-    
-
-    
   );
-
-  
 }
-
-
 
 // function Profile() {
 //     return (
@@ -217,9 +212,7 @@ const styles = StyleSheet.create({
     flex: 1,
     // backgroundColor: 'turquoise',
   },
-  card:{
-    
-  },
+  card: {},
   userInfoSection: {
     paddingHorizontal: 30,
     marginBottom: 25,
